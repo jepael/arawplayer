2e1ca160b065d26399c891d00d50785667989c82
jepael <jepael@users.noreply.github.com>
2016-07-16 03:13:03 +0300

    Update Code::Blocks project options and make git ignore files

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void print_usage(void)
 {
-    printf("Hello world!\n");
+    printf( "\n"
+            "Usage : arawplayer <file>\n"
+          );
+}
+
+int main(int argc, char *argv[])
+{
+    printf("RAW player\n");
+
+    if (argc != 2)
+    {
+        print_usage();
+        return 0;
+    }
+
+    printf("File name %s\n",argv[1]);
+
     return 0;
 }
